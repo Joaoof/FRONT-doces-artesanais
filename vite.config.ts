f11030a7899ebd7662ace15b0f509466fdc1e777
Joaoof <joaodeus400@gmail.com>
2025-01-15 17:24:11 -0300

    feat: fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/mnt/c/Users/joaod/Downloads/doce-demais/project/index.html', // Caminho para o arquivo index.html
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
